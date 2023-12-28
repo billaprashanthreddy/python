5fef2061fdc8c6047571a93b82b689aa45fdb3e2
billa@pc <billa@gmail.com>
2023-12-29 01:44:26 +0530

    nn

---
+++ conditinal.py
@@ -0,0 +1,19 @@
+# Example of a conditional statement
+x = 10
+
+if x > 0:
+    print("x is positive")
+elif x == 0:
+    print("x is zero")
+else:
+    print("x is negative")
+
+# Example of a for loop
+for i in range(5):
+    print(i)
+# Example of a while loop
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
